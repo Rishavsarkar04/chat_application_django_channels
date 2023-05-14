aece7daf3c4bc4d05dde7985a35708324803b717
Rishav Sarkar <85944172+Rishavsarkar04@users.noreply.github.com>
2023-05-14 13:06:32 +0530

    Update app_filters.py

--- app/templatetags/app_filters.py
+++ app/templatetags/app_filters.py
@@ -12,7 +12,6 @@ def filter_seen(query, sender):
 
 @register.filter(name='filter_user_chat')
 def filter_user_chat(query,user):
-    print(user, 'filter')
     a = query.filter(Q(sender=user) | Q(receiver=user))
     if a.exists():
         return True
